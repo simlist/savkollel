b028fc1a410abd0bc60570b70c21b157508068fb
Meir S. List <simlist@gmail.com>
2016-09-15 21:34:23 -0400

    Changed time zone and created models.

--- nlaws/models.py
+++ nlaws/models.py
@@ -3,3 +3,16 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+
+class Produce(models.Model):
+    product = CharField(max_length=100)
+
+class Order(models.Model):
+    order_date = models.DateField()
+    customer = ForeignKey(Customer)
+    produce = ForeignKey(Produce)
+    quantity = IntegerField()
+
