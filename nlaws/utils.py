abbefacd524d6b2fee672ef3cea1019f1aa5897f
Meir S. List <simlist@gmail.com>
2016-09-25 09:51:09 -0400

    Added utils file. Function for combining dicts.

---
+++ nlaws/utils.py
@@ -0,0 +1,22 @@
+"""This module contains utilities needed in the nlaws app"""
+
+def _inner_merge_dicts(dicts):
+    if len(dicts) > 1:
+        dict0 = dicts.pop()
+        dict1 = dicts.pop().copy()
+        for key in dict0:
+            val = dict1.pop(key, 0)
+            dict0[key] += val
+        dict0.update(dict1)
+        dicts.append(dict0)
+        return _inner_merge_dicts(dicts)
+    else:
+        return dicts[0]
+
+def merge_dicts(*dicts):
+    """Combine multiple dictionaries adding the values of duplicate keys. 
+    """
+    dicts = list(dicts)
+    dict0 = dicts.pop().copy()
+    dicts.append(dict0)
+    return _inner_merge_dicts(dicts)
