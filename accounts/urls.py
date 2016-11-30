c39950683e9387282fcdcb032af1f19fd4cdad36
Meir S. List <simlist@gmail.com>
2016-11-29 23:43:22 -0500

    Setup login and logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
+from . import views
+
 urlpatterns = [url('login', auth_views.login, 
                    {'template_name': 'accounts/login.html'}, name='login'),
+               url('logout', auth_views.logout,
+                   {'template_name': 'accounts/logout.html'}, name='logout'),
+               url('profile', views.profile)
     ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
+def profile(request):
+#change to home
+    return redirect('/nlaws/')
