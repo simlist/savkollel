98eab4bdfa14eb798708be94ce92a740cfb2bf1d
Meir S. List <simlist@gmail.com>
2016-11-29 23:48:20 -0500

    Rethought models model.

--- nlaws/models.py
+++ nlaws/models.py
@@ -1,18 +1,21 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Customer(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=100)
 
-class Produce(models.Model):
-    product = models.CharField(max_length=100)
 
 class Order(models.Model):
     order_date = models.DateField()
-    customer = models.ForeignKey(Customer)
-    produce = models.ForeignKey(Produce)
-    quantity = models.IntegerField(default=0)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.BooleanField(default=False)
+
+class Invoice(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    produce = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=1)
+
