803fb3eb7206a70d575acf50683d755719f5e274
Meir S. List <simlist@gmail.com>
2016-12-13 20:57:02 -0500

    Added models to admin

--- nlaws/admin.py
+++ nlaws/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product
+from .models import *
 
-admin.site.register(Product)
+models = [Product, Order, Invoice]
+admin.site.register(models)
