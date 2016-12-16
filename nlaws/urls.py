0ae62a9d5d4a468d43097c21b75fa2e5dee25ca8
Meir S. List <simlist@gmail.com>
2016-12-15 21:40:31 -0500

    Added combine url

--- nlaws/urls.py
+++ nlaws/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 app_name = 'nlaws'
 urlpatterns = [url(r'^$', index, name='index'),
-               url(r'^shoppinglist$', ShoppingList.as_view(), name='shoppinglist')
+               url(r'^shoppinglist$', ShoppingList.as_view(), name='shoppinglist'),
+               url(r'^combine$', Combine.as_view(), name='combine')
                ]
