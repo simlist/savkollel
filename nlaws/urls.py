ef76d6b3c54defa06adad088e4dd7d25df041007
Meir S. List <simlist@gmail.com>
2016-12-30 08:35:46 -0500

    Fixed url regex endings

--- nlaws/urls.py
+++ nlaws/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [url(r'^$', index, name='index'),
                url(r'^shoppinglist$', ShoppingList.as_view(), name='shoppinglist'),
                url(r'^combine$', Combine.as_view(), name='combine'),
                url(r'^viewlist$', ViewList.as_view(), name='viewlist'),
-               url(r'^orderslist', ViewOrders.as_view(), name='orderslist'),
-               url(r'nlaws/deleteorder', DeleteOrder.as_view(), name='deleteorder')
+               url(r'^orderslist$', ViewOrders.as_view(), name='orderslist'),
+               url(r'nlaws/deleteorder$', DeleteOrder.as_view(), name='deleteorder')
                ]
